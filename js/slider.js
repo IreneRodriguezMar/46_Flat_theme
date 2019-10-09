addf23124304a983888f5d64e4b8c105462d047f
IreneRodriguezMar <irene.rodriguezmar@gmail.com>
2019-10-09 16:45:52 -0500

    section three

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+//slider section three
+$('.section-three-slider-projects-cont').slick({
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 300
+});
